eaf05887d449afea7787d040f70cb009b9051902
GidoVW <gido_vanwijk@hotmail.com>
2016-11-25 14:30:59 +0100

    Supression commentaires

--- Sources/application/main.c
+++ Sources/application/main.c
@@ -8,16 +8,43 @@
 #include "BCM_appli.h"
 #include "DCM_appli.h"
 
-
-
 /*************** Private function prototype **********/
 
-//************** Public function *************/
+/*************** Public function            **********/
 
  int main(void) {
 
-				
+	 disableWatchdog();
+	 initModesAndClock();
+	 
+	 /*
+	 SIU.PCR[68].R = 0x0200;// LED EN Sortie
+	 SIU.PCR[69].R = 0x0200;
+	 SIU.PCR[70].R = 0x0200;
+	 SIU.PCR[65].R = 0x0100; // S2 en entree
+	 SIU.PCR[64].R = 0x0100;
+	 SIU.PCR[66].R = 0x0100;
+	 while(1) {
+		 if(SIU.GPDI[65].B.PDI == 1) {
+			SIU.GPDO[68].R = 1; 
+		 }
+		 else SIU.GPDO[68].R = 0;
+		 
+		 if(SIU.GPDI[66].B.PDI == 1){
+			 SIU.GPDO[70].R= 1;}
+		 else SIU.GPDO[70].R = 0;
+		 
+		 if(SIU.GPDI[64].B.PDI == 1)
+			 
+		 {   SIU.GPDO[69].R = 1;
+		 }
+			else SIU.GPDO[69].R = 0;
+		
+	 }   */
 }
+	 
+				
+
 
 
 
